aeb5647a1432068f045285563dd9c10e9b14db23
Udoka <udochukwukaonyela@gmail.com>
2023-07-01 08:43:44 +0100

    Refactor swap API handler code
    The commit refactors the swap API handler code to improve readability and maintainability. It moves the connection configuration to environment variables and adds logging for debugging purposes. Additionally, it updates the contract function calls to the new syntax and removes unnecessary console logs.

--- src/pages/api/swap.ts
+++ src/pages/api/swap.ts
@@ -6,142 +6,44 @@ export default async function handler(
 ) {
   const { method } = req;
   const { amountIn, tokenIn, tokenOut, executerAdress } = req.body;
-  const swapContract = new ethers.Contract(
-    "0x426964a065b8A3cdf4bb7Bbc9AA583e76669BA0a",
-    `
-  [
-    {
-      "inputs": [],
-      "name": "poolFee",
-      "outputs": [
-        {
-          "internalType": "uint24",
-          "name": "",
-          "type": "uint24"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "routerAddress",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "amountIn",
-          "type": "uint256"
-        },
-        {
-          "internalType": "address",
-          "name": "tokenIn",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "tokenOut",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "executerAdress",
-          "type": "address"
-        }
-      ],
-      "name": "swapExactInputSingle",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "amountOut",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "amountOut",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256",
-          "name": "amountInMaximum",
-          "type": "uint256"
-        },
-        {
-          "internalType": "address",
-          "name": "tokenIn",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "tokenOut",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "executerAdress",
-          "type": "address"
-        }
-      ],
-      "name": "swapExactOutputSingle",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "amountIn",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "swapRouter",
-      "outputs": [
-        {
-          "internalType": "contract ISwapRouter",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
-  `
-  );
-
+  const url = process.env.INFURA_GOERLI_ENDPOINT
+  const privateKey = process.env.privateKey
+  // var signer = new ethers.Wallet(privateKey, connection);
   switch (method) {
     case "POST":
       try {
-        const tx = await swapContract?.swapExactInputSingle?.(
+        const provider = new ethers.JsonRpcProvider(url);
+        const signer = new ethers.Wallet(privateKey ?? "", provider);
+        console.log(executerAdress);
+        const swapContract = new ethers.Contract(
+          "0x426964a065b8A3cdf4bb7Bbc9AA583e76669BA0a",
+          [
+          "function swapExactInputSingle(uint256 amountIn, address tokenIn, address tokenOut, address executerAdress) external returns (uint256 amountOut)",
+          "function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum, address tokenIn, address tokenOut, address executerAdress) external returns (uint256 amountIn)"],
+          signer
+        );
+        console.log(signer.provider?.getBalance(signer.address))
+        console.log(swapContract?.getFunction("swapExactInputSingle"))
+console.log({amountIn,
+          tokenIn,
+          tokenOut,
+          executerAdress})
+        const tx = await swapContract?.swapExactInputSingle
+          ?.(
           amountIn,
           tokenIn,
           tokenOut,
           executerAdress
         );
-        res.status(200).json({ success: true, data: tx });
+        res.status(200).json({ success: true, data: null });
       } catch (error) {
         console.log(error);
+        res.status(500).json({
+          success: false
+        })
       }
       break;
     default:
-      console.log(swapContract.interface.fragments);
       res.status(400).json({ success: false });
       break;
   }
